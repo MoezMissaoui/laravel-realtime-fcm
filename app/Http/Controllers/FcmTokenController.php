0aa113e892aea22acc7676fca1d1fbb04d62f553
moezmissaoui <moeiz.missaoui@gmail.com>
2023-09-06 10:24:46 +0100

    cleaning code

--- app/Http/Controllers/FcmTokenController.php
+++ app/Http/Controllers/FcmTokenController.php
@@ -10,14 +10,10 @@ use Illuminate\Http\Request;
 
 class FcmTokenController extends Controller
 {
-    
-
 
 
     public function store(Request $request)
     {
-
-
         $currentUserInfo = Location::get($request->ip());
 
         if(!FcmToken::where('token',$request->token)->exists()){
